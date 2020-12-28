164c262518e90df1ac133a8fc100793bfcabb3e3
Claudio Nogueira <cllaudionoggueira@gmail.com>
2020-12-28 02:01:40 -0300

    First commit

---
+++ Dictionary01/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Dictionary01 {
+    class Program {
+        static void Main(string[] args) {
+            Dictionary<string, int> Voting = new Dictionary<string, int>();
+            try {
+                Console.WriteLine("Enter the full path:");
+                string path = Console.ReadLine();
+                using (StreamReader sr = File.OpenText(path)) {
+                    while (!sr.EndOfStream) {
+                        string[] lines = sr.ReadLine().Split(',');
+                        string candidate = lines[0];
+                        int votes = int.Parse(lines[1]);
+                        if (Voting.ContainsKey(candidate)) {
+                            Voting[candidate] += votes;
+                        }
+                        else {
+                            Voting[candidate] = votes;
+                        }
+                    }
+                }
+                foreach (KeyValuePair<string, int> item in Voting) {
+                    Console.WriteLine(item.Key + ": " + item.Value);
+                }
+            }
+            catch (IOException e) {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
